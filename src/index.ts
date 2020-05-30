4167d7b6dc062aa631deadb31075dc40265f1b64
lllllllqw <465633856@qq.com>
2020-05-30 17:55:01 +0800

    add: schema

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-import { parse, visit } from 'graphql'
+import { graphql, parse, visit, buildASTSchema, GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql'
+
+const UserType = new GraphQLObjectType({
+  name: 'user',
+  fields: {
+    name: {
+      type: GraphQLString,
+    },
+    uuid: {
+      type: GraphQLString
+    }
+  }
+})
+
+const AppGraphQLSchema = new GraphQLSchema({
+  query: UserType
+})
 
 const query = `
   query USER_LIST {
@@ -11,6 +27,10 @@ const query = `
 `
 
 const ast = parse(query)
+
+graphql({
+  schema: 
+}) //?
 
 ast
 
---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+declare interface Database {
+  getTable: (tableName) => Table
+}
+
+declare interface Table {
+
+}
